7d6c2de09a2f560cdef64c67afc29a30994cb66b
LaMonte <lnunn6@emich.edu>
2024-02-13 16:29:32 -0500

    "Added basic vue+vite webpage"

---
+++ path_finding/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+  <h1> - MAP GOES HERE - </h1>
+
+  <div class="card">
+    <p>
+      
+
+Basic Map Display <br>
+Zoom and Pan
+Click start and destination<br>
+Basic Routing <br>
+Select algorithm to compute path<br>
+Dijkstra’s<br>
+A-Star (A*)<br>
+BFS<br>
+DFS<br>
+Real-time visual analysis of the algorithm working<br>
+Path is clearly displayed<br>
+Analytics (run-time, number of nodes in the path)
+
+
+
+
+    </p>
+  </div>
+
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
