bba524eeaba9dbbb7addeb46ad75c934df0421f1
rKAmindo <randykamindo@gmail.com>
2024-03-05 14:41:52 -0500

    This is python script parses the compressed XML OSM data, which contains data of a region
    Currently we are parsing an XML format, This
    is currently pretty slow, and I will explore
    parsing the PBF format which is a much lighterweight
    file. Currently we have extracted the data into
    an undirected graph
    
    issue #2

--- OSM Converter.py
+++ OSM Converter.py
@@ -1,26 +1,60 @@
-import os
 import xml.etree.ElementTree as ET
 import lzma
+import json
+from collections import deque
 
 def read_osm(osm_file):
     adjacency_list = {}
     with lzma.open(osm_file, 'rb') as f:
         tree = ET.parse(f)
         root = tree.getroot()
-        # Process the parsed XML data
-        for node in root.findall('.//node'):
+        # Extract nodes and their coordinates
+        for node in root.findall('node'):
             node_id = node.attrib['id']
             lat = float(node.attrib['lat'])
             lon = float(node.attrib['lon'])
             adjacency_list[node_id] = {'lat': lat, 'lon': lon, 'neighbors': []}
-        # Rest of your code
+        # Extract ways and their nodes (to build neighbors list)
+        for way in root.findall('way'):
+            way_nodes = [nd.attrib['ref'] for nd in way.findall('nd')]
+            for i in range(len(way_nodes) - 1):
+                node1 = way_nodes[i]
+                node2 = way_nodes[i + 1]
+                if node1 not in adjacency_list[node2]['neighbors']:
+                    adjacency_list[node2]['neighbors'].append(node1)
+                if node2 not in adjacency_list[node1]['neighbors']:
+                    adjacency_list[node2]['neighbors'].append(node2)
     return adjacency_list
 
 # Example usage
 file_name = 'TESTFILE.osm.xz'
-file_path = os.path.abspath(file_name)
 
-adj_list = read_osm(file_path)
-print(adj_list)
+def extract_largest_connected_graph(graph: dict):
+    # initialize queue, visited set
+    visited = set()
+    cc = {}
 
+    def bfs(v):
+        queue = deque([v])
+        while queue:
+            for neighbor in queue[0]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+                queue.popleft()
+    
+    for node in graph:
+        if node not in visited:
+            bfs(node)
+        cc[node] = visited
+        print(cc[node])
+        
+   
+
+
+adj_list = read_osm(file_name)
+with open('data.json', 'w') as outfile:
+    json.dump(adj_list, outfile)
+
+extract_largest_connected_graph(adj_list)
 
