004704c1e75f1bf663384ca46c3c142754428d80
thNarr8or <thNarr8or@gmail.com>
2024-03-12 15:59:16 -0400

    Fixed bugs in BFS algorithm, errors, and other logic errors from using typescript. Also changed the configuration to allow for Map interactions.

--- app/Algorithms/BFS.ts
+++ app/Algorithms/BFS.ts
@@ -6,37 +6,51 @@ Takes a starting point on the map, and finds a path to the end location
 by using a breadth-first search. Also requires a Map (aka Hash table) with
 all of the nodes on the map provided
 */
-function BFS(string: start, string: end, Map: nodes){
-	
+import { nodeInfo } from "../types.ts";
+
+
+
+function BFS(start: string, end: string, nodes: Map<string, nodeInfo>) {
+
 	//Hash table to store which vertices have been visited. Keys are the previous node in the path
-	const visited = new Map();
-	
-	const queue = [start];
-	
+	const visited: Map<string,string> = new Map<string, string>();
+
+	const queue: string[] = [start];
+
+	visited.set(start, "start");
+
+
 	//Loop until end is found
-	while (queue.length > 0){
-		
+	while (queue.length > 0) {
+
+		const current: string = queue[0];
+		const currentNode: nodeInfo = nodes.get(current);
+
 		//Found the end!
-		if(nodes.get(queue[0]).equals(end)){
-			
-			return findPath(end,visited);
+		if (current === (end)) {
+
+			return findPath(end, visited);
 		}
-		
+
+
 		//For each node adjacent to queued element
-		for(let i: number = 0; i < nodes.get(queue[0]).adj.length; i++){
+		for (let i: number = 0; i < currentNode.adj.length; i++) {
+
 			//If the adjacent nodes to the queued element have not been visited yet
-			if(!visited.has(nodes.get(queue[0].adj[i]))){
-				
+			if (!visited.has(currentNode.adj[i])) {
+
 				//marks current adjacent node with where it came from (use vertex names for hash table)
 				//Key is the adj node's identity, the value of it is the current node's identity
 				//i.e. if 1 goes to 2 and 2 to 3, the hash table would look like {[2,1], [3,2]} at end of program
-				visited.set(queue[0].adj[i],queue[0]);
-				
+				visited.set(currentNode.adj[i], queue[0]);
+
 				//Adds adjacent node to queue
-				queue.push(nodes.get(queue[0].adj[i]));
+				queue.push(currentNode.adj[i]);
 			}
-		}	
-		
+		}
+
+
+
 		//Removes this element from queue
 		queue.shift();
 	}
@@ -44,21 +58,25 @@ function BFS(string: start, string: end, Map: nodes){
 	return null;
 }
 
+
+
 //Recursively finds the path and returns a path list
-function findPath(string: current, Map: visited){
-	
+function findPath(current: string, visited: Map<string, string>){
+
 	//If this element has a node that the path came through before this
-	if(visited.has(visited.get(current))){
-		
+	if (visited.get(current) === ("start")) {
+
 		//Recursive call to find beginning of the path,
 		//returns a list with ordering of path
-		return findPath(visited.get(current), visited).push(current);
-		
+		return findPath(visited.get(current), visited).push(current); // needs to add current element to recursive call list
+
 	}
-	
+
 	//Base case: hash table does not have parent element, i.e. the start
-	else{
+	else {
 		//Returns a list with the starting location at index 0
 		return [current];
 	}
 }
+
+const example: Map<string, nodeInfo> = new Map();
