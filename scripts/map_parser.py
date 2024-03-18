9e6a6336728eee063b760b5b251c7967df30bafb
rKAmindo <randykamindo@gmail.com>
2024-03-18 16:50:06 -0400

    Modify map_parser.py script to properly handle one-way streets. Implement directional parsing logic to ensure accurate representation of street networks.
    We check if the Way is tagged as a one_way or not.
    For every two adjacent nodes in the list of nodes
    of a Way, node1 and node2. If the Way is a one_way
    There exists the directed edge (node1, node2), if
    it is not a one way, there exists the directed
    edges (node1, node2) and (node2, node1). We add the
    corresponding adjacent nodes to the graph based
    on this property.
    
    issue #18

--- scripts/map_parser.py
+++ scripts/map_parser.py
@@ -34,7 +34,7 @@ default_speeds = {
 
 def read_osm(osm_file):
     print('Processing OSM file data...')
-    adjacency_list = {}
+    routing_graph = {}
     class NodeHandler(osmium.SimpleHandler):
         
         def node(self, n):
@@ -44,7 +44,7 @@ def read_osm(osm_file):
             Args:
                 n (osmium.osm.Node): OSM node object.
             """
-            adjacency_list[str(n.id)] = {'lat': n.location.lat, 'lon': n.location.lon, 'adj': []}
+            routing_graph[str(n.id)] = {'lat': n.location.lat, 'lon': n.location.lon, 'adj': []}
 
         def way(self, w):
             """
@@ -60,21 +60,49 @@ def read_osm(osm_file):
                                     'motorway_link', 'trunk_link', 'primary_link', 
                                     'secondary_link', 'tertiary_link', 'living_street', 'service']:
                     nodes = w.nodes
+
+                    # determine if the way is one-way
+                    is_one_way = 'oneway' in w.tags and w.tags['oneway'] in ['yes', 'true', '1']
                     for i in range(len(nodes) - 1):
                         node1_id = str(nodes[i].ref)
                         node2_id = str(nodes[i + 1].ref)
-                        # Get the latitude and longitude of the current and next nodes
-                        lat1, lon1 = adjacency_list[node1_id]['lat'], adjacency_list[node1_id]['lon']
-                        lat2, lon2 = adjacency_list[node2_id]['lat'], adjacency_list[node2_id]['lon']
 
+                        # get the coordinates of the two nodes adjacent to each other
+                        lat1, lon1 = routing_graph[node1_id]['lat'], routing_graph[node1_id]['lon']
+                        lat2, lon2 = routing_graph[node2_id]['lat'], routing_graph[node2_id]['lon']
+
+                        # calculate Haversine distance in miles
                         distance = haversine((lat1, lon1), (lat2, lon2), unit=Unit.MILES)
+
+                        # calulate the time it takes to travel between the adjacent nodes
                         max_speed = self.get_max_speed(w, highway_type)
                         time = distance / max_speed # time in hours
-                        if node1_id not in adjacency_list[node2_id]['adj']:
-                            adjacency_list[node2_id]['adj'].append({'nodeId': node1_id, 'distance': distance, 'time': time})
-                        if node2_id not in adjacency_list[node1_id]['adj']:
-                            adjacency_list[node1_id]['adj'].append({'nodeId': node2_id, 'distance': distance, 'time': time})
+                        if node2_id not in routing_graph[node1_id]['adj']: 
+                            routing_graph[node1_id]['adj'].append({'nodeId': node2_id, 'distance': distance, 'time': time})
 
+                        if not is_one_way and node1_id not in routing_graph[node2_id]['adj']:
+                            routing_graph[node2_id]['adj'].append({'nodeId': node1_id, 'distance': distance, 'time': time})
+
+
+        def is_node_in_adj(self, graph, node_id, target_node_id):
+            """
+            Function to check if a target node is in the adjacency list of a given node.
+
+            Args:
+                graph (dict): Adjacency list representing the graph.
+                node_id (str): The node id to check.
+                target_node_id (str): The target node id to search for in the adjacency list.
+
+            Returns:
+                bool: True if the target node is found in the adjacency list of the given node, False otherwise.
+            """
+            if node_id in graph:
+                for neighbor in graph[node_id]['adj']:
+                    if neighbor['nodeId'] == target_node_id:
+                        return True
+            return False
+             
+                        
         def get_max_speed(self, w, highway_type):
             """
             Helper function to get the maximum speed from OSM way tags.
@@ -102,7 +130,7 @@ def read_osm(osm_file):
 
     handler = NodeHandler()
     handler.apply_file(osm_file)
-    return adjacency_list
+    return routing_graph
 
 def extract_largest_connected_graph(graph: dict):
     print('Extracting the largest component...')
@@ -146,7 +174,14 @@ def extract_largest_connected_graph(graph: dict):
     largest_component_graph = {n: graph[n] for n in largest_component}  # Build largest subgraph            
     
     print("Size of the largest component:", len(largest_component_graph), 'nodes')
+    print("Total number of edges:", count_edges(largest_component_graph))
     return largest_component_graph
+
+def count_edges(graph):
+    total_edges = 0
+    for node_id, node_data in graph.items():
+        total_edges += len(node_data['adj'])
+    return total_edges
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -156,8 +191,8 @@ if __name__ == '__main__':
     output_file = input('Enter the output file name (should be a .json): ')
     print('Running...')
     file_name = sys.argv[1]
-    adj_list = read_osm(file_name)
-    largest_component = extract_largest_connected_graph(adj_list)
+    graph = read_osm(file_name)
+    largest_component = extract_largest_connected_graph(graph)
     with open(f'../public/data/{output_file}', 'w') as outfile:
         json.dump(largest_component, outfile)
         print(f'Generating {output_file}')
