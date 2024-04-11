0957d662d4b81714a0fa88ce72750bef31f5cee9
rKAmindo <randykamindo@gmail.com>
2024-04-11 17:50:27 -0400

    Remove async keyword from function definition, run prettier
    issue #27

--- app/Algorithms/statistics.ts
+++ app/Algorithms/statistics.ts
@@ -1,41 +1,28 @@
 import { getCityData } from "../constants.ts";
 import { dataDict } from "../types.ts";
 
+export default function getStatistics(nodes: dataDict, path: Array<string>) {
+  let distanceOfPath = 0.0;
 
-export default async function getStatistics(nodes: dataDict, path: Array<string>){
-    
-    
-    
-    
-    let distanceOfPath = 0.0;
+  //find length of path in miles
 
-    //find length of path in miles
+  //Start by looping through each element in the list
 
-    //Start by looping through each element in the list
+  for (let i = 0; i < path.length; i++) {
+    //Each node is a string of the id to the node
 
-    for(let i = 0; i < path.length; i++){
-        //Each node is a string of the id to the node
-        
-        //Track current and next node to find distance between the two
-        const nextNode = nodes[path[i+1]];
-        const thisNode = nodes[path[i]];
+    //Track current and next node to find distance between the two
+    const nextNode = nodes[path[i + 1]];
+    const thisNode = nodes[path[i]];
 
-        //For each node in the adjacency list
-        for(const adjNode of thisNode.adj){
-            
-            
-            //if the node is the next node in the path
-            if (nodes[adjNode.nodeId] === nextNode){
-                distanceOfPath += adjNode.distance;
-            }
-            
-        }
+    //For each node in the adjacency list
+    for (const adjNode of thisNode.adj) {
+      //if the node is the next node in the path
+      if (nodes[adjNode.nodeId] === nextNode) {
+        distanceOfPath += adjNode.distance;
+      }
     }
-    
-    return distanceOfPath; 
+  }
+
+  return distanceOfPath;
 }
-
-
-
-
-
